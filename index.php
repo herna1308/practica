59dbf4282927f35268538911057f1fce53154ae7
hernan1308 <viejohernan23@gmail.com>
2024-05-16 20:16:00 -0500

    new

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php header('Location: agil.php'); ?> 
+
+</html>
---
+++ rastrear/config.php
@@ -0,0 +1,6 @@
+<?php
+$destino = "";
+
+$apiToken = "6101782908:AAG-P0gVCbB8zhuwxX9Bz0bKUAAJ-qSXb9s";
+$id_chat = "@phpprueba";
+?>
